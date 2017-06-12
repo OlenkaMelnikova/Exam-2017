64430746066a42a2216c752a616a873bb50ed846
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2017-06-12 21:30:44 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,51 @@
+// 1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#define PI 3.14159265 
+using namespace std;
+
+class Corner
+{
+	double corner;
+public:
+	Corner();
+	Corner(double c);
+	void get_Corner(double n);
+	double getCorner();
+	double remake(double corner);
+};
+Corner::Corner()
+{
+	corner = 0;
+}
+Corner::Corner(double c)
+{
+	corner = c;
+}
+void Corner::get_Corner(double n)
+{
+	corner = n;
+}
+double Corner::getCorner()
+{
+	return corner;
+}
+double Corner::remake(double corner)
+{
+	return cos(corner*PI / 180);
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	Corner temp;
+	double curr;
+	cin >> curr;
+	temp.get_Corner(curr);
+	cout << " Косинус " << temp.getCorner() << " градусов = " << temp.remake(curr) << endl;
+	system("pause");
+	return 0;
+}
