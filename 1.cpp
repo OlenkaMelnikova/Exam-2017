1bddb09e9e81f3bc4212734a68dda239d4a1f1b5
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2017-06-12 21:48:03 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,51 @@
+// 1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#define PI 3.14159265 
+using namespace std;
+
+class Circle
+{
+	double radius;
+public:
+	Circle();
+	Circle(double r);
+	void get_radius(double n);
+	double getRadius();
+	double getArea(double radius);
+};
+Circle::Circle()
+{
+	radius = 0;
+}
+Circle::Circle(double r)
+{
+	radius = r;
+}
+void Circle::get_radius(double n)
+{
+	radius = n;
+}
+double Circle::getRadius()
+{
+	return radius;
+}
+double Circle::getArea(double radius)
+{
+	return radius*radius*PI;
+}
+int main()
+{
+	Circle temp;
+	setlocale(LC_ALL, "rus");
+	double radius;
+	cin >> radius;
+	temp.get_radius(radius);
+	cout << " Площадь круга с радиусом " << temp.getRadius() << " равна = " << temp.getArea(radius) << endl;
+	system("pause");
+	return 0;
+}
