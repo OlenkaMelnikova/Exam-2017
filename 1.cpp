4bd3a816cff6439a2491963426c6698e390a577e
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2017-06-12 23:04:03 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,60 @@
+// 1.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class integer
+{
+	int number;
+public:
+	integer();
+	integer(int n);
+	void get_number(int m);
+	int getNumber();
+	string inttohex(int number);
+};
+integer::integer()
+{
+	number = 0;
+}
+integer::integer(int n)
+{
+	number = n;
+}
+void integer::get_number(int m)
+{
+	number = m;
+}
+int integer::getNumber()
+{
+	return number;
+}
+string integer::inttohex(int number)
+{
+	string tmp("");
+	do
+	{
+		int r(number % 16);
+		if (r>9) { r += (int)'A' - 10; }
+		else { r += (int)'0'; };
+		tmp = (char)r + tmp;
+		number /= 16;
+	} while (number);
+	return tmp;
+}
+
+int main()
+{
+	integer temp;
+	int number;
+	setlocale(LC_ALL, "rus");
+	cin >> number;
+	temp.get_number(number);
+	cout << "„исло " << temp.getNumber() << " в шестнадцатиричной системе счислени€ : " << temp.inttohex(number) << endl;
+	system("pause");
+	return 0;
+}
